f1d97d1d180e11fc83b4780b4ef154c8eb336cf1
davershayan12 <davershayan13@gmail.com>
2025-03-10 00:48:31 +0500

    .

---
+++ basic_java_progaming/step06Loops/DoWhileloops/DoWhileloop.java
@@ -0,0 +1,12 @@
+package basic_java_progaming.step06Loops.DoWhileloops;
+
+public class DoWhileloop {
+    public static void main (String[] args){
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+    }
+    
+}
