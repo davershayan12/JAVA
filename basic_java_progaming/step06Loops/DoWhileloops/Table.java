86011a8b31bb06402579b5e581be1141ab3dd36a
davershayan12 <davershayan13@gmail.com>
2025-03-10 00:53:15 +0500

    .

---
+++ basic_java_progaming/step06Loops/DoWhileloops/Table.java
@@ -0,0 +1,18 @@
+package basic_java_progaming.step06Loops.DoWhileloops;
+
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] var0) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a table number:");
+        int number = input.nextInt();
+        System.out.println("\n");
+        input.close();
+        int i=1;
+        do{
+            System.out.println(number + "x" +i + "=" + number * i);
+        }while(i<=10);   
+        
+     }
+}
