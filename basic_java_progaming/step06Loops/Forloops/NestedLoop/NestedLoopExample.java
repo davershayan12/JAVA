50b50f8ee7555455ca8e556ab37fdb718ce3504d
davershayan12 <davershayan13@gmail.com>
2025-03-14 16:58:09 +0500

    .

--- basic_java_progaming/step06Loops/Forloops/NestedLoop/NestedLoopExample.java
+++ basic_java_progaming/step06Loops/Forloops/NestedLoop/NestedLoopExample.java
@@ -1,6 +1,13 @@
 package basic_java_progaming.step06Loops.Forloops.NestedLoop;
 
 public class NestedLoopExample {
-    public Static  
+    public static void main(String[]args){
+        for (int i =1;i<=10;i++){
+            for(int j=1; j<=10;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
     
 }
