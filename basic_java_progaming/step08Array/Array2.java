2c44dfe5d32fc4d156dc170cb4d1ed69d2182dd0
davershayan12 <davershayan13@gmail.com>
2025-03-15 18:30:12 +0500

    .

--- basic_java_progaming/step08Array/Array2.java
+++ basic_java_progaming/step08Array/Array2.java
@@ -1,14 +1,21 @@
 package basic_java_progaming.step08Array;
 import java.util.Scanner;
+
 public class Array2 {
-    public static void main(String[] args){
-        int[] numbers=new int[10];
-        Scanner input=new Scanner(System.in);
-        for(int i=0;i<10;i++){
-            numbers[i]=input.nextInt();
+    public static void main(String[] args) {
+        
+        int[] numbers = new int[10];
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter 10 numbers:");
+        for (int i = 0; i < 10; i++) { 
+            numbers[i] = input.nextInt();
         }
         input.close();
-        System.out.println(numbers);
+
+        System.out.println("You entered:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
     }
-    
 }
