0009272c643679c4e158030dba4a0a808b7a8017
davershayan12 <davershayan13@gmail.com>
2025-03-18 05:32:09 +0500

    .

--- basic_java_progaming/step09StringMethod/ComparingString.java
+++ basic_java_progaming/step09StringMethod/ComparingString.java
@@ -8,12 +8,7 @@ public class ComparingString {
         System.out.println(s2.equalsIgnoreCase(s2));//Returns true if this string is equal to string s1; it is case insensitive.
         System.out.println(s2.compareTo(s2));//Returns an integer greater than 0, equal to 0, or less than 0 to indicate whether this string is greater than, equal to, or less than s1.
         System.out.println(s2.compareToIgnoreCase(s2));//Same as compareTo except that the comparison is case insensitive.
-
-
-
-
-
-    }
+        }
     
 
     
